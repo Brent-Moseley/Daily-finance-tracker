d448bf6fd76f10b781e19ec4777876a4e6a41f56
Brent-Moseley <techwrite88@gmail.com>
2015-05-25 13:29:11 -0700

    temp

--- app/models/user.js
+++ app/models/user.js
@@ -7,9 +7,17 @@ console.log (' Defining User model....');
 
 
 // define the User model
+// When letting the user add a new category, will have to add it to the string array below
+// and add a new category record for it in the catgeory model so it can have a limit.
+// UI controller will define the default set of categories for new users
+// and will recurse through the user's category array to populate drop downs and the popup.
+// Will also need the category delete to remove from the array and the category model.
+// That plus an add button on the popup.
+
 module.exports = mongoose.model('User', {
       userName : {type : String, default: ''},
       pwd : {type : String, default: ''},
+      categories: [String],
       key: {type : String, default: 'demo'}    // key is like a user ID
     },
     'users');    // collection name
@@ -18,3 +26,4 @@ module.exports = mongoose.model('User', {
 //  MongoDB data types:
 // http://docs.mongodb.org/manual/core/document/
 // http://docs.mongodb.org/manual/reference/bson-types/
+// Mongoose Schema types:  http://mongoosejs.com/docs/schematypes.html 
